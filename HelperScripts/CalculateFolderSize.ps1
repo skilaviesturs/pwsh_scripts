f2ab07d745917aa38c96861f52aa17dd51e26664
skilaviesturs <skila.viesturs@gmail.com>
2022-03-07 10:54:27 +0200

    improve code

--- HelperScripts/CalculateFolderSize.ps1
+++ HelperScripts/CalculateFolderSize.ps1
@@ -5,17 +5,15 @@ Param (
 )
 
 $dataColl = @()
-Get-ChildItem -force $Path -ErrorAction SilentlyContinue | Where-Object { $_ -is [io.directoryinfo] } | ForEach-Object {
+$dataColl = Get-ChildItem -force $Path -ErrorAction SilentlyContinue | Where-Object { $_ -is [io.directoryinfo] } | ForEach-Object {
 	$len = 0
 	Get-ChildItem -recurse -force $_.fullname -ErrorAction SilentlyContinue | Where-Object { $len += $_.length }
-	$foldername = $_.fullname
-	$foldersize= '{0:N2}' -f ($len / 1Mb)
-	$dataObject = New-Object PSObject
-	Add-Member -inputObject $dataObject -memberType NoteProperty -name 'FolderName' -value $foldername
-	Add-Member -inputObject $dataObject -memberType NoteProperty -name 'FolderSize' -value $foldersize
-	$dataColl += $dataObject
+	New-Object -TypeName psobject -Property @{
+		foldername = $_.fullname
+		foldersize = '{0:N2}' -f ($len / 1Mb)
+	}
 }
 #$dataColl | Get-Member
 $dataColl | Format-Table FolderName,@{name='Folder size (Mb)';expression={$_.FolderSize};align='right'} -AutoSize
 #$dataColl | Select-Object -Property FolderName,@{name='Folder size (Mb)';expression={$_.FolderSize}} | Out-GridView
-#$dataColl | Select-Object -Property FolderName,@{name='Folder size (Mb)';expression={$_.FolderSize}} | Export-CSV result.txt
+#$dataColl | Select-Object -Property FolderName,@{name='Folder size (Mb)';expression={$_.FolderSize}} | Export-CSV result.csv -notypeinformation -delimiter ";" -encoding UTF8
